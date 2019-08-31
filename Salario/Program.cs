63641592d3b19101a0c1bce491be5451324b8ae6
anolol <anoemangi@gmail.com>
2019-08-31 15:37:11 -0300

    The Methode exemplo

---
+++ Salario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalarioMedio mm = new SalarioMedio();
+            SalarioMedio mn = new SalarioMedio();
+
+            Console.WriteLine("Dados do primeiro funcinário");
+            Console.Write("Ditige seu Nome completo: ");
+            mm.Nome = Console.ReadLine();
+            Console.Write("salário: ");
+            mm.salario = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do segundo funcionário");
+            Console.Write("Digite seu nome completo: ");
+            mn.Nome = Console.ReadLine();
+            mn.salario = decimal.Parse(Console.ReadLine());
+
+            decimal medio = (mm.salario + mn.salario) / 2;
+
+            Console.WriteLine("Salário medio é : " + medio);
+
+        }
+    }
+}
---
+++ Salario/SalarioMedio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salario
+{
+    class SalarioMedio
+    {
+        public string Nome;
+        public decimal salario;
+
+
+    }
+}
